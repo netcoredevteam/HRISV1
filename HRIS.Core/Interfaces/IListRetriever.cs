ae1c4e68c0e20ad09c5252450283e15a6ed17a5d
Cristos <User@DESKTOP-AAFJSPF>
2022-08-31 10:53:31 +0800

    added core library

---
+++ HRIS.Core/Interfaces/IListRetriever.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRIS.Core.Interfaces
+{
+    internal interface IListRetriever<Entity>
+    {
+        Task<IEnumerable<Entity>> GetAllAsync();
+    }
+}
---
+++ HRIS.Core/Interfaces/IUpdater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRIS.Core.Interfaces
+{
+    internal interface IUpdater<Entity>
+    {
+        Task UpdateAsync(Entity entity);
+    }
+}
