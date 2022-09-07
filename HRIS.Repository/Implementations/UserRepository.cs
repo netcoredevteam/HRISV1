53b322dc6c114598006fb14a656385d355fc8a04
Jerez Orayle <jerezorayle@gmail.com>
2022-09-07 16:19:17 +0800

    Added User Repository

---
+++ HRIS.Repository/Implementations/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using HRIS.Domain.Entities;
+using HRIS.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRIS.Repository.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task DeleteAsync(User entity)
+        {
+            _context.Users.Remove(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetAsync(Guid id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task InsertAsync(User entity)
+        {
+            _context.Users.Add(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User entity)
+        {
+            _context.Users.Update(entity);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ HRIS.Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using HRIS.Core.Interfaces;
+using HRIS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRIS.Repository.Interfaces
+{
+    public interface IUserRepository : IDeleter<User>,
+                                       IInserter<User>,
+                                       IListRetriever<User>,
+                                       IRetriever<User, Guid>,
+                                       IUpdater<User>
+    {
+    }
+}
