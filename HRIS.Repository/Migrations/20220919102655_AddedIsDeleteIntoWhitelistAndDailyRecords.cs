e2bb384699ccf0609bb3cf5ddf520c677c6c6905
Jerez Orayle <jerezorayle@gmail.com>
2022-09-19 18:30:24 +0800

    Migration

---
+++ HRIS.Repository/Migrations/20220919102655_AddedIsDeleteIntoWhitelistAndDailyRecords.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRIS.Repository.Migrations
+{
+    public partial class AddedIsDeleteIntoWhitelistAndDailyRecords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "whitelists",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "daily_records",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "whitelists");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "daily_records");
+        }
+    }
+}
