0634cdb978ac52de0e8380bc29546e96de22a655
Jerez Orayle <jerezorayle@gmail.com>
2022-09-19 15:04:33 +0800

    Added nickname to UserDto

--- HRIS.Service/DTOs/UserDto.cs
+++ HRIS.Service/DTOs/UserDto.cs
@@ -11,6 +11,7 @@ namespace HRIS.Service.DTOs
     {
         public Guid Id { get; set; }
         public string? Username { get; set; }
+        public string? Nickname { get; set; }
         public string? Password { get; set; }
         public string? Role { get; set; }
         public Guid EmployeeId { get; set; }
--- HRIS.WebApi/Models/Request/CreateUserRequestModel.cs
+++ HRIS.WebApi/Models/Request/CreateUserRequestModel.cs
@@ -3,6 +3,7 @@
     public class CreateUserRequestModel
     {
         public string? Username { get; set; }
+        public string? Nickname { get; set; }
         public string? Password { get; set; }
         public string? Role { get; set; }
         public Guid EmployeeId { get; set; }
